baa5dd30e0e79c780199133322035af323b5a106
unknown <sina@sina.com>
2024-02-06 18:45:17 +0330

    last bug is fixed

--- components/BouncingCircle.vue
+++ components/BouncingCircle.vue
@@ -1,23 +1,44 @@
 <template>
     <div v-if="props.target == 'firstCircle'">
+        <span ref="circle1" class="hidden"></span>
+        <span ref="circle2" class="hidden"></span>
+        <span ref="circle3" class="hidden"></span>
+        <span ref="circle4" class="hidden"></span>
         <span ref="circle5" class="inline-block w-8 h-8 scale-[2] sm:w-12 sm:h-12 md:w-16 md:h-16 lg:w-[70px] lg:h-[70px] bg-black rounded-full"></span>
     </div>
     <div v-else-if="props.target == 'case'" class="flex items-center">
+        <span ref="circle1" class="hidden"></span>
+        <span ref="circle2" class="hidden"></span>
+        <span ref="circle3" class="hidden"></span>
+        <span ref="circle4" class="hidden"></span>
         <div ref="circle5" class="w-[22px] h-[22px] scale-[2] sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-[68px] lg:h-[68px] bg-black rounded-full"></div>
     </div>
     <div v-else-if="props.target == 'slug'" class="flex items-center">
         <span ref="circle1" class="inline-block w-[30px] h-[30px] sm:w-[37px] sm:h-[37px] scale-[2] md:w-[45px] md:h-[45px] lg:w-[70px] lg:h-[70px] bg-black rounded-full ml-3 mr-4 sm:mr-5 sm:ml-4 md:mx-6 lg:ml-8 lg:mr-8"></span>
+        <span ref="circle2" class="hidden"></span>
+        <span ref="circle3" class="hidden"></span>
+        <span ref="circle4" class="hidden"></span>
         <span ref="circle5" class="inline-block w-[30px] h-[30px] sm:w-[37px] sm:h-[37px] scale-[2] md:w-[45px] md:h-[45px] lg:w-[70px] lg:h-[70px] bg-black rounded-full ml-4 mr-3 sm:ml-5 sm:mr-4 md:mx-6 lg:ml-10 lg:mr-8"></span>
     </div>
     <div v-else-if="props.target == 'secondCircle'">
         <span ref="circle1" class="inline-block w-8 h-8 scale-[2] sm:w-12 sm:h-12 md:w-16 md:h-16 lg:w-[70px] lg:h-[70px] bg-black rounded-full"></span>
+        <span ref="circle2" class="hidden"></span>
+        <span ref="circle3" class="hidden"></span>
+        <span ref="circle4" class="hidden"></span>
+        <span ref="circle5" class="hidden"></span>
     </div>
     <div v-else-if="props.target == 'footer'">
+        <span ref="circle1" class="hidden"></span>
+        <span ref="circle2" class="hidden"></span>
+        <span ref="circle3" class="hidden"></span>
+        <span ref="circle4" class="hidden"></span>
         <span ref="circle5" :class="props.color" class="inline-block w-[28px] scale-[2] h-[28px] mr-3 ml-4 md:ml-10 md:mr-10 md:w-[70px] md:h-[70px] rounded-full"></span>
     </div>
     <div v-else class="relative w-[340px] h-[260px] lg:w-[500px] lg:h-[300px]">
         <span ref="circle1" class="absolute left-24 lg:left-48 top-0 w-12 h-12 scale-[2] lg:w-16 lg:h-16 bg-black rounded-full"></span>
         <span ref="circle2" class="absolute right-0 top-1/2 -translate-y-1/2 w-12 h-12 scale-[2] lg:w-16 lg:h-16 bg-black rounded-full"></span>
+        <span ref="circle3" class="hidden"></span>
+        <span ref="circle4" class="hidden"></span>
         <span ref="circle5" class="absolute left-24 lg:left-48 bottom-0 w-12 h-12 scale-[2] lg:w-16 lg:h-16 bg-black rounded-full"></span>
     </div>
 </template>
